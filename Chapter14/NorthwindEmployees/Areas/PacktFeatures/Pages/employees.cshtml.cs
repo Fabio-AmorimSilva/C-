deac90fa96b86978f87f73c8cfc158422178ef4d
Fabio <fabioamorim2025@hotmail.com>
2021-04-12 17:32:36 -0300

    Adicionada página de empregadosvia ligação com banco de dados

---
+++ Chapter14/NorthwindEmployees/Areas/PacktFeatures/Pages/employees.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NorthwindContextLib;
+using Packt.CS7;
+
+namespace NorthwindEmployees.PacktFeatures.Pages
+{
+    public class EmployeesPageModel : PageModel
+    {
+
+        private Northwind db;
+        public EmployeesPageModel(Northwind injectedContext){
+
+            db = injectedContext;
+
+        }
+
+        public IEnumerable<string> Employees{get; set;}
+        public void OnGet()
+        {
+
+            Employees = db.Employees.Select(e => e.FirstName);
+
+        }
+    }
+}
