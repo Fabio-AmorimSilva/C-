374fe51a43a307361481c9e54ed674cec3554d8d
unknown <fabioamorim2025@hotmail.com>
2022-02-11 16:59:20 -0300

    Correção de código

--- Carl-Hugo-Marcotte/Chapter3/OpenClosed/Final Program/Ninja.cs
+++ Carl-Hugo-Marcotte/Chapter3/OpenClosed/Final Program/Ninja.cs
@@ -3,6 +3,11 @@ namespace Final_Program
 
     public interface IAttackble{};
 
+    public AttackResult Attack(IAttackble target)
+    {
+        return new AttackResult(EquippedWeapon, this, target);
+    }
+
     public class Ninja : IAttackble
     {
         public Weapon EquippedWeapon{get; set;}
